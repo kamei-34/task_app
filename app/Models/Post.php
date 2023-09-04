f97116981920a13801c88505a8c1fb443fa79ed6
kamei-34 <rakuzira.34.it@gmail.com>
2023-09-05 02:55:36 +0900

    修正

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,7 +12,7 @@ class Post extends Model
     use HasFactory;
     public function getByLimit(int $limit_count = 10)
     {
-        return $this->orderBy('updated_at','DESC')->limit($limit_count)->get();
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
     
     protected $fillable = [
